openshift/ovn-kubernetes

Get route advertisement information in NAD controller
The network manager running within the NAD controller will, upon
ensuring a network, fetch the VRFs per node a pod network is being
leaked/advertised to from the applicable route advertisements
configuration, and include it in the network information used when
creating a network controller, or triggering a reconciliation if it was
already running.

This relies on annotations set by cluster manager on NADs pointing to
the route advertising configuration that applies to the network which
will come in a future PR/commit.

This includes the default network for which the ever existing default
network controller is used (instead of creating a new network
controller). If necessary, it is assumed that cluster manager will
create a dummy NAD for the default network in ovn-k namespace to set
annotations on. If no NADs for the default network exist or if they have
no annotations, network manager will reconcile the default network to a
default configuration (instead of destroying the network controller).

Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

go-controller/pkg/network-attach-def-controller/network_attach_def_controller_test.go
@@ -127,9 +127,8 @@ func TestNetAttachDefinitionController(t *testing.T) {
 	}
 
 	network_Default := &ovncnitypes.NetConf{
-		Topology: types.Layer3Topology,
 		NetConf: cnitypes.NetConf{
-			Name: "default",
+			Name: types.DefaultNetworkName,
 			Type: "ovn-k8s-cni-overlay",
 		},
 		MTU: 1400,
@@ -150,14 +149,19 @@ func TestNetAttachDefinitionController(t *testing.T) {
 		expected []expected
 	}{
 		{
-			name: "NAD on default network should be skipped",
+			name: "NAD on default network is tracked with default controller",
 			args: []args{
 				{
 					nad:     "test/nad_1",
 					network: network_Default,
 				},
 			},
-			expected: []expected{},
+			expected: []expected{
+				{
+					network: network_Default,
+					nads:    []string{"test/nad_1"},
+				},
+			},
 		},
 		{
 			name: "NAD added",
@@ -382,14 +386,19 @@ func TestNetAttachDefinitionController(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			g := gomega.NewWithT(t)
+
 			tncm := &testNetworkControllerManager{
 				controllers: map[string]NetworkController{},
+				defaultNetwork: &testNetworkController{
+					NetInfo: &util.DefaultNetInfo{},
+				},
 			}
-			nadController := &NetAttachDefinitionController{
+			nadController := &NADController{
 				networks:       map[string]util.NetInfo{},
 				nads:           map[string]string{},
-				networkManager: newNetworkManager("", tncm),
+				networkManager: newNetworkManager("", "", "", tncm, nil),
 			}
+			nm := nadController.networkManager.(*networkManagerImpl)
 
 			g.Expect(nadController.networkManager.Start()).To(gomega.Succeed())
 			defer nadController.networkManager.Stop()
@@ -414,24 +423,36 @@ func TestNetAttachDefinitionController(t *testing.T) {
 			}
 
 			meetsExpectations := func(g gomega.Gomega) {
+				nm.Lock()
+				defer nm.Unlock()
 				tncm.Lock()
 				defer tncm.Unlock()
 
 				var expectRunning []string
+				g.Expect(nm.networks).To(gomega.HaveLen(len(tt.expected)))
 				for _, expected := range tt.expected {
 					netInfo, err := util.NewNetInfo(expected.network)
 					g.Expect(err).ToNot(gomega.HaveOccurred())
 
+					// test that the desired networks have the expected config and NADs
 					name := netInfo.GetNetworkName()
-					testNetworkKey := testNetworkKey(netInfo)
-
-					// test that the controller have the expected config and NADs
-					g.Expect(tncm.controllers).To(gomega.HaveKey(testNetworkKey))
-					g.Expect(tncm.controllers[testNetworkKey].Equals(netInfo)).To(gomega.BeTrue(),
+					g.Expect(nm.networks).To(gomega.HaveKey(name))
+					g.Expect(nm.networks[name].Equals(netInfo)).To(gomega.BeTrue(),
 						fmt.Sprintf("matching network config for network %s", name))
-					g.Expect(tncm.controllers[testNetworkKey].GetNADs()).To(gomega.ConsistOf(expected.nads),
+					g.Expect(nm.networks[name].GetNADs()).To(gomega.ConsistOf(expected.nads),
 						fmt.Sprintf("matching NADs for network %s", name))
-					expectRunning = append(expectRunning, testNetworkKey)
+
+					// test that the actual secondary controllers have the
+					// expected config and NADs
+					if name != types.DefaultNetworkName {
+						testNetworkKey := testNetworkKey(netInfo)
+						g.Expect(tncm.controllers).To(gomega.HaveKey(testNetworkKey))
+						g.Expect(tncm.controllers[testNetworkKey].Equals(netInfo)).To(gomega.BeTrue(),
+							fmt.Sprintf("matching network config for network controller %s", name))
+						g.Expect(tncm.controllers[testNetworkKey].GetNADs()).To(gomega.ConsistOf(expected.nads),
+							fmt.Sprintf("matching NADs for network controller %s", name))
+						expectRunning = append(expectRunning, testNetworkKey)
+					}
 				}
 				expectStopped := sets.New(tncm.started...).Difference(sets.New(expectRunning...)).UnsortedList()
 
@@ -502,7 +523,7 @@ func TestSyncAll(t *testing.T) {
 			tncm := &testNetworkControllerManager{
 				controllers: map[string]NetworkController{},
 			}
-			nadController, err := NewNetAttachDefinitionController(
+			nadController, err := NewClusterNADController(
 				"SUT",
 				tncm,
 				wf,

go-controller/pkg/network-attach-def-controller/network_manager_test.go
@@ -1,0 +1,177 @@
+package networkAttachDefController
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onsi/gomega"
+
+	cnitypes "github.com/containernetworking/cni/pkg/types"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
+
+	ovncnitypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/cni/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
+	ratypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/routeadvertisements/v1"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
+)
+
+func TestSetVRFs(t *testing.T) {
+	testZoneName := "testZone"
+	testNodeName := "testNode"
+	testNodeOnZoneName := "testNodeOnZone"
+	testNADName := "test/NAD"
+	testRAName := "testRA"
+	testVRFName := "testVRF"
+
+	defaultNetwork := &ovncnitypes.NetConf{
+		NetConf: cnitypes.NetConf{
+			Name: types.DefaultNetworkName,
+			Type: "ovn-k8s-cni-overlay",
+		},
+		MTU: 1400,
+	}
+
+	podNetworkRA := &ratypes.RouteAdvertisements{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testRAName,
+		},
+		Spec: ratypes.RouteAdvertisementsSpec{
+			TargetVRF: testVRFName,
+			Advertisements: ratypes.Advertisements{
+				PodNetwork: true,
+			},
+		},
+	}
+	nonPodNetworkRA := &ratypes.RouteAdvertisements{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testRAName,
+		},
+		Spec: ratypes.RouteAdvertisementsSpec{
+			TargetVRF: testVRFName,
+		},
+	}
+
+	testNode := corev1.Node{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testNodeName,
+		},
+	}
+	testNodeOnZone := corev1.Node{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testNodeOnZoneName,
+			Annotations: map[string]string{
+				util.OvnNodeZoneName: testZoneName,
+			},
+		},
+	}
+	otherNode := corev1.Node{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "otherNode",
+		},
+	}
+
+	tests := []struct {
+		name     string
+		network  *ovncnitypes.NetConf
+		ra       *ratypes.RouteAdvertisements
+		node     corev1.Node
+		expected map[string][]string
+	}{
+		{
+			name:    "reconciles VRF for selected node of default node network controller",
+			network: defaultNetwork,
+			ra:      podNetworkRA,
+			node:    testNode,
+			expected: map[string][]string{
+				testNodeName: {testVRFName},
+			},
+		},
+		{
+			name:    "reconciles VRF for selected node in same zone as default OVN network controller",
+			network: defaultNetwork,
+			ra:      podNetworkRA,
+			node:    testNodeOnZone,
+			expected: map[string][]string{
+				testNodeOnZoneName: {testVRFName},
+			},
+		},
+		{
+			name:    "ignores a route advertisement that is not for the pod network",
+			network: defaultNetwork,
+			ra:      nonPodNetworkRA,
+			node:    testNode,
+		},
+		{
+			name:    "ignores a route advertisement that is not for applicable node",
+			network: defaultNetwork,
+			ra:      podNetworkRA,
+			node:    otherNode,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := gomega.NewWithT(t)
+
+			config.OVNKubernetesFeature.EnableMultiNetwork = true
+			config.OVNKubernetesFeature.EnableRouteAdvertisements = true
+			fakeClient := util.GetOVNClientset().GetOVNKubeControllerClientset()
+			wf, err := factory.NewOVNKubeControllerWatchFactory(fakeClient)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+
+			tncm := &testNetworkControllerManager{
+				controllers: map[string]NetworkController{},
+				defaultNetwork: &testNetworkController{
+					NetInfo: &util.DefaultNetInfo{},
+				},
+			}
+			nm := newNetworkManager("", testZoneName, testNodeName, tncm, wf)
+
+			namespace, name, err := cache.SplitMetaNamespaceKey(testNADName)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			nad, err := buildNAD(name, namespace, tt.network)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			nad.Annotations = map[string]string{
+				util.OvnRouteAdvertisements: "[\"" + tt.ra.Name + "\"]",
+			}
+
+			_, err = fakeClient.KubeClient.CoreV1().Nodes().Create(context.Background(), &tt.node, v1.CreateOptions{})
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			_, err = fakeClient.RouteAdvertisementsClient.K8sV1().RouteAdvertisements().Create(context.Background(), tt.ra, v1.CreateOptions{})
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			_, err = fakeClient.NetworkAttchDefClient.K8sCniCncfIoV1().NetworkAttachmentDefinitions(namespace).Create(context.Background(), nad, v1.CreateOptions{})
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+
+			err = wf.Start()
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			defer wf.Shutdown()
+			g.Expect(nm.Start()).To(gomega.Succeed())
+			defer nm.Stop()
+
+			netInfo, err := util.NewNetInfo(tt.network)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			netInfo.AddNADs(testNADName)
+
+			nm.EnsureNetwork(netInfo)
+
+			meetsExpectations := func(g gomega.Gomega) {
+				var reconcilable util.ReconcilableNetInfo
+				switch tt.network.Name {
+				case types.DefaultNetworkName:
+					reconcilable = tncm.GetDefaultNetworkController().(util.ReconcilableNetInfo)
+				default:
+					reconcilable = tncm.controllers[testNetworkKey(netInfo)]
+				}
+
+				g.Expect(reconcilable).ToNot(gomega.BeNil())
+				g.Expect(reconcilable.GetVRFs()).To(gomega.Equal(tt.expected))
+			}
+
+			g.Eventually(meetsExpectations)
+			g.Consistently(meetsExpectations)
+		})
+	}
+}

go-controller/pkg/network-controller-manager/node_network_controller_manager.go
@@ -34,7 +34,7 @@ type nodeNetworkControllerManager struct {
 
 	// net-attach-def controller handle net-attach-def and create/delete secondary controllers
 	// nil in dpu-host mode
-	nadController *nad.NetAttachDefinitionController
+	nadController *nad.NADController
 }
 
 // NewNetworkController create secondary node network controllers for the given NetInfo
@@ -75,12 +75,14 @@ func NewNodeNetworkControllerManager(ovnClient *util.OVNClientset, wf factory.No
 
 	// need to configure OVS interfaces for Pods on secondary networks in the DPU mode
 	var err error
-	if config.OVNKubernetesFeature.EnableMultiNetwork && config.OvnKubeNode.Mode == ovntypes.NodeModeDPU {
-		ncm.nadController, err = nad.NewNetAttachDefinitionController("node-network-controller-manager", ncm, wf)
+	if (config.OVNKubernetesFeature.EnableMultiNetwork && config.OvnKubeNode.Mode == ovntypes.NodeModeDPU) ||
+		(config.OVNKubernetesFeature.EnableRouteAdvertisements && config.OvnKubeNode.Mode == ovntypes.NodeModeFull) {
+		ncm.nadController, err = nad.NewNodeNADController("node-network-controller-manager", ncm, wf, name)
 	}
 	if err != nil {
 		return nil, err
 	}
+
 	return ncm, nil
 }
 
@@ -132,17 +134,21 @@ func (ncm *nodeNetworkControllerManager) Start(ctx context.Context) (err error) 
 	if err != nil {
 		return fmt.Errorf("failed to init default node network controller: %v", err)
 	}
+
+	// nadController is nil if multi-network is disabled
+	if ncm.nadController != nil {
+		err = ncm.nadController.Start()
+		if err != nil {
+			return fmt.Errorf("failed to start default node network NAD controller: %v", err)
+		}
+	}
+
 	err = ncm.defaultNodeNetworkController.Start(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to start default node network controller: %v", err)
 	}
 
-	// nadController is nil if multi-network is disabled
-	if ncm.nadController != nil {
-		err = ncm.nadController.Start()
-	}
-
-	return err
+	return nil
 }
 
 // Stop gracefully stops all managed controllers

jcaamano/ovn-kubernetes-downstream

go-controller/pkg/network-attach-def-controller/network_attach_def_controller_test.go
@@ -141,9 +141,8 @@ func TestNetAttachDefinitionController(t *testing.T) {
 	}
 
 	networkDefault := &ovncnitypes.NetConf{
-		Topology: types.Layer3Topology,
 		NetConf: cnitypes.NetConf{
-			Name: "default",
+			Name: types.DefaultNetworkName,
 			Type: "ovn-k8s-cni-overlay",
 		},
 		MTU: 1400,
@@ -164,14 +163,19 @@ func TestNetAttachDefinitionController(t *testing.T) {
 		expected []expected
 	}{
 		{
-			name: "NAD on default network should be skipped",
+			name: "NAD on default network is tracked with default controller",
 			args: []args{
 				{
 					nad:     "test/nad_1",
 					network: networkDefault,
 				},
 			},
-			expected: []expected{},
+			expected: []expected{
+				{
+					network: networkDefault,
+					nads:    []string{"test/nad_1"},
+				},
+			},
 		},
 		{
 			name: "NAD added",
@@ -444,12 +448,16 @@ func TestNetAttachDefinitionController(t *testing.T) {
 			config.OVNKubernetesFeature.EnableMultiNetwork = true
 			tncm := &testNetworkControllerManager{
 				controllers: map[string]NetworkController{},
+				defaultNetwork: &testNetworkController{
+					NetInfo: &util.DefaultNetInfo{},
+				},
 			}
-			nadController := &NetAttachDefinitionController{
+			nadController := &nadController{
 				nads:           map[string]string{},
-				networkManager: newNetworkManager("", tncm),
 				primaryNADs:    map[string]string{},
+				networkManager: newNetworkManager("", "", "", tncm, nil),
 			}
+			nm := nadController.networkManager.(*networkManagerImpl)
 
 			g.Expect(nadController.networkManager.Start()).To(gomega.Succeed())
 			defer nadController.networkManager.Stop()
@@ -475,31 +483,48 @@ func TestNetAttachDefinitionController(t *testing.T) {
 
 			meetsExpectations := func(g gomega.Gomega) {
 				var expectRunning []string
+				g.Expect(nm.networks).To(gomega.HaveLen(len(tt.expected)))
 				for _, expected := range tt.expected {
 					netInfo, err := util.NewNetInfo(expected.network)
 					g.Expect(err).ToNot(gomega.HaveOccurred())
 
+					// test that the desired networks have the expected config and NADs
 					name := netInfo.GetNetworkName()
 					testNetworkKey := testNetworkKey(netInfo)
 					func() {
+						nm.Lock()
+						defer nm.Unlock()
 						tncm.Lock()
 						defer tncm.Unlock()
-						// test that the controller have the expected config and NADs
-						g.Expect(tncm.controllers).To(gomega.HaveKey(testNetworkKey))
-						g.Expect(tncm.controllers[testNetworkKey].Equals(netInfo)).To(gomega.BeTrue(),
+
+						// test that the manager has all the desired networks,
+						// including the default network if NAD/VRF changes had
+						// to be reconciled
+						g.Expect(nm.networks).To(gomega.HaveKey(name))
+						g.Expect(nm.networks[name].Equals(netInfo)).To(gomega.BeTrue(),
 							fmt.Sprintf("matching network config for network %s", name))
-						g.Expect(tncm.controllers[testNetworkKey].GetNADs()).To(gomega.ConsistOf(expected.nads),
+						g.Expect(nm.networks[name].GetNADs()).To(gomega.ConsistOf(expected.nads),
 							fmt.Sprintf("matching NADs for network %s", name))
+
+						// test that the actual controllers have the expected config and NADs
+						if !netInfo.IsDefault() {
+							g.Expect(tncm.controllers).To(gomega.HaveKey(testNetworkKey))
+							g.Expect(tncm.controllers[testNetworkKey].Equals(netInfo)).To(gomega.BeTrue(),
+								fmt.Sprintf("matching network config for network %s", name))
+							g.Expect(tncm.controllers[testNetworkKey].GetNADs()).To(gomega.ConsistOf(expected.nads),
+								fmt.Sprintf("matching NADs for network %s", name))
+						}
 					}()
 					expectRunning = append(expectRunning, testNetworkKey)
 					if netInfo.IsPrimaryNetwork() && !netInfo.IsDefault() {
-						netInfo.SetNADs(expected.nads...)
 						key := expected.nads[0]
 						namespace, _, err := cache.SplitMetaNamespaceKey(key)
 						g.Expect(err).ToNot(gomega.HaveOccurred())
 						netInfoFound, err := nadController.GetActiveNetworkForNamespace(namespace)
 						g.Expect(err).ToNot(gomega.HaveOccurred())
+						g.Expect(netInfoFound.Equals(netInfo)).To(gomega.BeTrue())
 						g.Expect(reflect.DeepEqual(netInfoFound, netInfo)).To(gomega.BeTrue())
+						g.Expect(netInfoFound.GetNADs()).To(gomega.ConsistOf(expected.nads))
 					}
 				}
 				tncm.Lock()
@@ -577,7 +602,7 @@ func TestSyncAll(t *testing.T) {
 			tncm := &testNetworkControllerManager{
 				controllers: map[string]NetworkController{},
 			}
-			nadController, err := NewNetAttachDefinitionController(
+			nadController, err := NewClusterNADController(
 				"SUT",
 				tncm,
 				wf,

go-controller/pkg/network-attach-def-controller/network_manager_test.go
@@ -1,0 +1,182 @@
+package networkAttachDefController
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onsi/gomega"
+
+	cnitypes "github.com/containernetworking/cni/pkg/types"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
+
+	ovncnitypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/cni/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
+	ratypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/routeadvertisements/v1"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
+)
+
+func TestSetVRFs(t *testing.T) {
+	testZoneName := "testZone"
+	testNodeName := "testNode"
+	testNodeOnZoneName := "testNodeOnZone"
+	testNADName := "test/NAD"
+	testRAName := "testRA"
+	testVRFName := "testVRF"
+
+	defaultNetwork := &ovncnitypes.NetConf{
+		NetConf: cnitypes.NetConf{
+			Name: types.DefaultNetworkName,
+			Type: "ovn-k8s-cni-overlay",
+		},
+		MTU: 1400,
+	}
+
+	podNetworkRA := &ratypes.RouteAdvertisements{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testRAName,
+		},
+		Spec: ratypes.RouteAdvertisementsSpec{
+			TargetVRF: testVRFName,
+			Advertisements: ratypes.Advertisements{
+				PodNetwork: true,
+			},
+		},
+	}
+	nonPodNetworkRA := &ratypes.RouteAdvertisements{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testRAName,
+		},
+		Spec: ratypes.RouteAdvertisementsSpec{
+			TargetVRF: testVRFName,
+		},
+	}
+
+	testNode := corev1.Node{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testNodeName,
+		},
+	}
+	testNodeOnZone := corev1.Node{
+		ObjectMeta: v1.ObjectMeta{
+			Name: testNodeOnZoneName,
+			Annotations: map[string]string{
+				util.OvnNodeZoneName: testZoneName,
+			},
+		},
+	}
+	otherNode := corev1.Node{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "otherNode",
+		},
+	}
+
+	tests := []struct {
+		name     string
+		network  *ovncnitypes.NetConf
+		ra       *ratypes.RouteAdvertisements
+		node     corev1.Node
+		expected map[string][]string
+	}{
+		{
+			name:    "reconciles VRF for selected node of default node network controller",
+			network: defaultNetwork,
+			ra:      podNetworkRA,
+			node:    testNode,
+			expected: map[string][]string{
+				testNodeName: {testVRFName},
+			},
+		},
+		{
+			name:    "reconciles VRF for selected node in same zone as default OVN network controller",
+			network: defaultNetwork,
+			ra:      podNetworkRA,
+			node:    testNodeOnZone,
+			expected: map[string][]string{
+				testNodeOnZoneName: {testVRFName},
+			},
+		},
+		{
+			name:    "ignores a route advertisement that is not for the pod network",
+			network: defaultNetwork,
+			ra:      nonPodNetworkRA,
+			node:    testNode,
+		},
+		{
+			name:    "ignores a route advertisement that is not for applicable node",
+			network: defaultNetwork,
+			ra:      podNetworkRA,
+			node:    otherNode,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := gomega.NewWithT(t)
+
+			config.OVNKubernetesFeature.EnableMultiNetwork = true
+			config.OVNKubernetesFeature.EnableRouteAdvertisements = true
+			fakeClient := util.GetOVNClientset().GetOVNKubeControllerClientset()
+			wf, err := factory.NewOVNKubeControllerWatchFactory(fakeClient)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+
+			tncm := &testNetworkControllerManager{
+				controllers: map[string]NetworkController{},
+				defaultNetwork: &testNetworkController{
+					NetInfo: &util.DefaultNetInfo{},
+				},
+			}
+			nm := newNetworkManager("", testZoneName, testNodeName, tncm, wf)
+
+			namespace, name, err := cache.SplitMetaNamespaceKey(testNADName)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			nad, err := buildNAD(name, namespace, tt.network)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			nad.Annotations = map[string]string{
+				types.OvnRouteAdvertisementsKey: "[\"" + tt.ra.Name + "\"]",
+			}
+
+			_, err = fakeClient.KubeClient.CoreV1().Nodes().Create(context.Background(), &tt.node, v1.CreateOptions{})
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			_, err = fakeClient.RouteAdvertisementsClient.K8sV1().RouteAdvertisements().Create(context.Background(), tt.ra, v1.CreateOptions{})
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			_, err = fakeClient.NetworkAttchDefClient.K8sCniCncfIoV1().NetworkAttachmentDefinitions(namespace).Create(context.Background(), nad, v1.CreateOptions{})
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+
+			err = wf.Start()
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			defer wf.Shutdown()
+			g.Expect(nm.Start()).To(gomega.Succeed())
+			defer nm.Stop()
+
+			netInfo, err := util.NewNetInfo(tt.network)
+			g.Expect(err).ToNot(gomega.HaveOccurred())
+			netInfo.AddNADs(testNADName)
+
+			nm.EnsureNetwork(netInfo)
+
+			meetsExpectations := func(g gomega.Gomega) {
+				tncm.Lock()
+				defer tncm.Unlock()
+				var reconcilable util.ReconcilableNetInfo
+				switch tt.network.Name {
+				case types.DefaultNetworkName:
+					reconcilable = tncm.GetDefaultNetworkController().(util.ReconcilableNetInfo)
+				default:
+					reconcilable = tncm.controllers[testNetworkKey(netInfo)]
+				}
+
+				g.Expect(reconcilable).ToNot(gomega.BeNil())
+				if tt.expected == nil {
+					tt.expected = map[string][]string{}
+				}
+				g.Expect(reconcilable.GetVRFs()).To(gomega.Equal(tt.expected))
+			}
+
+			g.Eventually(meetsExpectations).Should(gomega.Succeed())
+			g.Consistently(meetsExpectations).Should(gomega.Succeed())
+		})
+	}
+}

go-controller/pkg/network-controller-manager/node_network_controller_manager.go
@@ -40,7 +40,7 @@ type nodeNetworkControllerManager struct {
 
 	// net-attach-def controller handle net-attach-def and create/delete secondary controllers
 	// nil in dpu-host mode
-	nadController *nad.NetAttachDefinitionController
+	nadController nad.NADController
 	// vrf manager that creates and manages vrfs for all UDNs
 	vrfManager *vrfmanager.Controller
 	// route manager that creates and manages routes
@@ -110,6 +110,7 @@ func (ncm *nodeNetworkControllerManager) newCommonNetworkControllerInfo() *node.
 // (2) primary user defined networks is enabled (all modes)
 func isNodeNADControllerRequired() bool {
 	return (config.OVNKubernetesFeature.EnableMultiNetwork && config.OvnKubeNode.Mode == ovntypes.NodeModeDPU) ||
+		(config.OVNKubernetesFeature.EnableRouteAdvertisements && config.OvnKubeNode.Mode == ovntypes.NodeModeFull) ||
 		util.IsNetworkSegmentationSupportEnabled()
 }
 
@@ -129,9 +130,10 @@ func NewNodeNetworkControllerManager(ovnClient *util.OVNClientset, wf factory.No
 
 	// need to configure OVS interfaces for Pods on secondary networks in the DPU mode
 	// need to start NAD controller on node side for programming gateway pieces for UDNs
+	// need to start NAD controller on node side for VRF awareness with BGP
 	var err error
 	if isNodeNADControllerRequired() {
-		ncm.nadController, err = nad.NewNetAttachDefinitionController("node-network-controller-manager", ncm, wf, nil)
+		ncm.nadController, err = nad.NewNodeNADController("node-network-controller-manager", name, ncm, wf)
 		if err != nil {
 			return nil, err
 		}
@@ -145,8 +147,7 @@ func NewNodeNetworkControllerManager(ovnClient *util.OVNClientset, wf factory.No
 
 // initDefaultNodeNetworkController creates the controller for default network
 func (ncm *nodeNetworkControllerManager) initDefaultNodeNetworkController() error {
-	defaultNodeNetworkController, err := node.NewDefaultNodeNetworkController(ncm.newCommonNetworkControllerInfo(),
-		ncm.nadController)
+	defaultNodeNetworkController, err := node.NewDefaultNodeNetworkController(ncm.newCommonNetworkControllerInfo(), ncm.nadController)
 	if err != nil {
 		return err
 	}
@@ -206,6 +207,15 @@ func (ncm *nodeNetworkControllerManager) Start(ctx context.Context) (err error) 
 	if err != nil {
 		return fmt.Errorf("failed to init default node network controller: %v", err)
 	}
+
+	// nadController is nil if multi-network is disabled
+	if ncm.nadController != nil {
+		err = ncm.nadController.Start()
+		if err != nil {
+			return fmt.Errorf("failed to start default node network NAD controller: %v", err)
+		}
+	}
+
 	err = ncm.defaultNodeNetworkController.Start(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to start default node network controller: %v", err)